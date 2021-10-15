6998ef19150f9145c00c165031f45409a7cc66ed
Tim Martin <tim@timmart.in>
2021-10-15 02:39:30 -0500

    Polish up

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # fourget
 
-yadda
+Download/scrape media files from 4chan threads
+
+## Features
+
+- fast concurrent downloading with asyncio
+- skip download if already it already exists locally
+- progress bar
+
+## Example
+
+```shell
+$ fourget https://boards.4channel.org/g/thread/76759434
+```
+
+```shell
+$ fourget --help
+```
--- fourget/__main__.py
+++ fourget/__main__.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
+
 import asyncio
+import hashlib
+import string
 import sys
+from base64 import b64decode
 from pathlib import Path
-from typing import Any, Optional
+from typing import Any, AsyncIterator, Optional
 
 import aiofiles
 import attr
@@ -10,13 +14,12 @@ import httpx
 import typer
 from tqdm import tqdm
 from yarl import URL
+
 from fourget import log
 
-
-@attr.s(frozen=True, auto_attribs=True, kw_only=True, order=False)
-class MediaDownload:
-    url: str
-    output_path: Path
+ALLOWED_FILE_NAME_CHARS = set(
+    string.ascii_letters + string.digits + " !#$%&'()+,-.;=@[]^_`{}~"
+)
 
 
 @attr.s(frozen=True, auto_attribs=True, kw_only=True, order=False)
@@ -24,9 +27,25 @@ class Post:
     board: str
     post_id: int
     subject_text: Optional[str]
-    media_timestamp: Optional[int]
-    media_extension: Optional[str]
-    file_size: Optional[int]
+    comment: Optional[str]
+    file: Optional[Post.File]
+
+    @attr.s(frozen=True, auto_attribs=True, kw_only=True, order=False)
+    class File:
+        timestamp: int
+        extension: str
+        size: int
+        md5: bytes
+        board: str
+        poster_stem: str
+
+        @property
+        def name(self) -> str:
+            return f"{self.timestamp}{self.extension}"
+
+        @property
+        def url(self) -> str:
+            return f"https://i.4cdn.org/{self.board}/{self.name}"
 
     @classmethod
     def from_json(cls, board: str, json_resp: dict[str, Any]) -> Post:
@@ -36,40 +55,81 @@ class Post:
         if "sub" in json_resp:
             subject_text = json_resp["sub"]
 
-        media_timestamp = None
-        if "tim" in json_resp:
-            media_timestamp = json_resp["tim"]
+        comment = None
+        if "com" in json_resp:
+            comment = json_resp["com"]
 
-        media_extension = None
+        file_timestamp = None
+        if "tim" in json_resp:
+            file_timestamp = json_resp["tim"]
+
+        file_extension = None
         if "ext" in json_resp:
-            media_extension = json_resp["ext"]
+            file_extension = json_resp["ext"]
 
         file_size = None
         if "fsize" in json_resp:
             file_size = json_resp["fsize"]
 
+        file_md5 = None
+        if "md5" in json_resp:
+            file_md5 = b64decode(json_resp["md5"])
+
+        file_poster_stem = None
+        if "filename" in json_resp:
+            file_poster_stem = json_resp["filename"]
+
+        if (
+            file_timestamp is None
+            or file_extension is None
+            or file_size is None
+            or file_md5 is None
+            or file_poster_stem is None
+        ):
+            file = None
+        else:
+            file = Post.File(
+                timestamp=file_timestamp,
+                extension=file_extension,
+                size=file_size,
+                md5=file_md5,
+                poster_stem=file_poster_stem,
+                board=board,
+            )
+
         return Post(
             board=board,
             post_id=post_id,
             subject_text=subject_text,
-            media_timestamp=media_timestamp,
-            media_extension=media_extension,
-            file_size=file_size,
+            comment=comment,
+            file=file,
         )
 
     @property
-    def media_filename(self) -> Optional[str]:
-        if self.media_timestamp is None or self.media_extension is None:
-            return None
+    def description(self) -> Optional[str]:
+        """
+        This method attempts to give some textual description of the thread, but note
+        that it not be possible.
 
-        return f"{self.media_timestamp}{self.media_extension}"
+        First, if there is a post subject, use the first 80 characters of it. If not and
+        if there is a comment, use the first 80 characters of the comment. Otherwise,
+        return None.
+        """
+        max_length = 80
 
-    @property
-    def media_url(self) -> Optional[str]:
-        if self.media_filename is None:
-            return None
+        if self.subject_text:
+            return self.subject_text[:max_length]
 
-        return f"https://i.4cdn.org/{self.board}/{self.media_filename}"
+        if self.comment:
+            return self.comment[:max_length]
+
+        return None
+
+
+@attr.s(frozen=True, auto_attribs=True, kw_only=True, order=False)
+class DownloadItem:
+    post_file: Post.File
+    output_dir: Path
 
 
 @attr.s(frozen=True, auto_attribs=True, kw_only=True, order=False)
@@ -89,38 +149,74 @@ class SiteURL:
         return f"https://a.4cdn.org/{self.board}/thread/{self.thread_id}.json"
 
 
+async def md5_path(path: Path, max_chunk_size: int = 2 ** 20) -> bytes:
+    async with aiofiles.open(path, "rb") as f:
+        md5_hash = hashlib.md5()
+        while data := await f.read(max_chunk_size):
+            md5_hash.update(data)
+        return md5_hash.digest()
+
+
+async def download_file(
+    client: httpx.AsyncClient, url: str, path: Path, max_chunk_size: int = 2 ** 20
+) -> AsyncIterator[int]:
+    """
+    Download url to path with client, yielding the length of the chunks downloaded as
+    we go.
+    """
+    async with client.stream("GET", url) as response:
+        async with aiofiles.open(path, "wb") as f:
+            async for chunk in response.aiter_bytes(max_chunk_size):
+                await f.write(chunk)
+                yield len(chunk)
+
+
+def file_name_santize(name: str) -> str:
+    return "".join(c for c in name if c in ALLOWED_FILE_NAME_CHARS)
+
+
 async def consumer(
-    queue: asyncio.Queue[MediaDownload],
+    queue: asyncio.Queue[DownloadItem],
     pbar: tqdm,
+    new_download_event: asyncio.Event,
 ) -> None:
     """
     TODO
     """
-    log.debug("Starting media download worker...")
+    log.debug("Starting file download worker...")
 
     async with httpx.AsyncClient() as client:
 
         while True:
-            media_download = await queue.get()
+            download_item = await queue.get()
 
-            async with client.stream("GET", media_download.url) as response:
-                async with aiofiles.open(media_download.output_path, "wb") as f:
-                    async for chunk in response.aiter_bytes():
-                        await f.write(chunk)
-                        pbar.update(len(chunk))
+            post_file = download_item.post_file
+
+            local_file_name = file_name_santize(
+                f"{post_file.timestamp} - {post_file.poster_stem}{post_file.extension}"
+            )
+
+            output_path = download_item.output_dir / local_file_name
+
+            if output_path.exists() and await md5_path(output_path) == post_file.md5:
+                log.info(f"Not downloading {post_file.url} because it already exists")
+                pbar.update(post_file.size)
+            else:
+                async for chunk_size in download_file(
+                    client=client, url=post_file.url, path=output_path
+                ):
+                    pbar.update(chunk_size)
+                log.info(f"{post_file.url} -> {output_path}")
+                new_download_event.set()
 
             queue.task_done()
-            pbar.set_postfix_str(
-                f"file={media_download.output_path.name[:10]: <10}", refresh=False
-            )
-            pbar.update()
-            log.info(f"{media_download.url} -> {media_download.output_path}")
+            pbar.set_postfix_str(f"file={output_path.name[:10]: <10}", refresh=False)
 
 
 async def producer(
     site_url: SiteURL,
     root_output_dir: Path,
-    queue: asyncio.Queue[MediaDownload],
+    queue: asyncio.Queue[DownloadItem],
     pbar: tqdm,
 ) -> None:
     """
@@ -135,24 +231,31 @@ async def producer(
         for post in json_body["posts"]
     ]
 
-    total_media_size = sum(post.file_size for post in posts if post.file_size)
-    pbar.total = total_media_size
+    total_file_size = sum(post.file.size for post in posts if post.file)
+    pbar.total = total_file_size
 
-    assert posts[0] and posts[0].subject_text
+    op = posts[0]
+    assert op
 
-    output_dir = root_output_dir / "4chan" / site_url.board / f"{posts[0].post_id}"
+    if op.description is None:
+        trailer = ""
+    else:
+        trailer = f" - {op.description}"
 
-    output_dir.mkdir(parents=True, exist_ok=True)
-    log.debug(f"Created output directory {output_dir}")
+    output_dir_name = file_name_santize(
+        f"4chan - {site_url.board} - {op.post_id}{trailer}"
+    )
+
+    output_dir = root_output_dir / output_dir_name
+
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+        log.debug(f"Created output directory {output_dir}")
 
     for post in posts:
-        if not post.media_url or not post.media_filename:
+        if post.file is None:
             continue
-        await queue.put(
-            MediaDownload(
-                url=post.media_url, output_path=output_dir / post.media_filename
-            )
-        )
+        await queue.put(DownloadItem(post_file=post.file, output_dir=output_dir))
 
     await queue.join()
 
@@ -166,10 +269,11 @@ async def process_all(
     """
     TODO
     """
-    log.info(f"Downloading media files from {site_url}")
+    log.info(f"Downloading files from {site_url.api_endpoint_url}")
 
-    queue: asyncio.Queue[MediaDownload] = asyncio.Queue(maxsize=queue_maxsize)
+    queue: asyncio.Queue[DownloadItem] = asyncio.Queue(maxsize=queue_maxsize)
     pbar = tqdm(unit="B", unit_scale=True, unit_divisor=1024, leave=False)
+    new_download_event = asyncio.Event()
 
     all_tasks = set()
 
@@ -178,6 +282,7 @@ async def process_all(
             consumer(
                 queue=queue,
                 pbar=pbar,
+                new_download_event=new_download_event,
             ),
         )
         all_tasks.add(consumer_task)
@@ -205,7 +310,7 @@ async def process_all(
 
     await asyncio.gather(*pending, return_exceptions=True)
 
-    return True
+    return new_download_event.is_set()
 
 
 app = typer.Typer()
@@ -217,7 +322,7 @@ def main(
     output_dir: Path = typer.Option(
         default=Path("."),
         help=(
-            "Directory in which to store downloaded media. Will be placed in "
+            "Directory in which to store downloaded media files. Will be placed in "
             'heirarchy of "<output_dir>/4chan/<board>/<post_id>"'
         ),
     ),
@@ -243,7 +348,7 @@ def main(
     """
     TODO
     """
-    found = asyncio.run(
+    new_download = asyncio.run(
         process_all(
             site_url=SiteURL.from_url(url),
             root_output_dir=output_dir,
@@ -253,10 +358,7 @@ def main(
         debug=asyncio_debug,
     )
 
-    if found:
-        sys.exit(0)
-
-    sys.exit(1)
+    sys.exit(bool(new_download))
 
 
 if __name__ == "__main__":
